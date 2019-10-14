e028b00885bddfc5d4cde230b88299aed224db20
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-14 17:28:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   
    :names => {
   :reading_railroad => {"mortgage_value" => "$100"},
   :pennsylvania_railroad => {"mortgage_value" => "$200"},
