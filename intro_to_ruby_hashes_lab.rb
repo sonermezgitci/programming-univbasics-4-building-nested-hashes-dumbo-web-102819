2617202abfe64e0a63302c288434006b89e9a178
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-14 17:57:47 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,6 @@
 def base_hash
-countries_and_capitals_of_the_world = {
-  :north_america => {
-    "Canada"=>{capital: "Ottawa", capital_climate: "Kőppen Dfb"
-     },
-    "USA" => {capital: "Washington D.C.", capital_climate: "Kőppen Cfa
-       },
-  },
-  :africa =>{
-    "Ghana"=> {capital: "Accra", capital_climate: "Kőppen Aw"
-  },
-    "Nigeria"=> {capital: "Abuja", capital_climate : "Kőppen Aw"
-      }
-  }
-}
-	{}
+{:railroads => {}}
+	
 end
 
 def monopoly_with_second_tier
@@ -21,6 +8,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+{:railroads => {:pieces => 4 }}
 end
 
 def monopoly_with_third_tier
@@ -28,11 +16,39 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  {:railroads => { 
+   :pieces => 4 ,
+  :rent_in_dollars =>{
+      :one_piece_owned => 25 ,
+      :two_pieces_owned => 50 ,
+      :three_pieces_owned => 100 ,
+      :four_pieces_owned => 200
+    },
+  :names => {
+  :reading_railroad => {},
+  :pennsylvania_railroad => {},
+  :b_and_o_railroad => {},
+  :shortline_railroad =>{}
+  }
+}}
 end
-
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   {:railroads => { 
+   :pieces => 4 ,
+  :rent_in_dollars =>{
+      :one_piece_owned => 25 ,
+      :two_pieces_owned => 50 ,
+      :three_pieces_owned => 100 ,
+      :four_pieces_owned => 200
+    },
+  :names => {
+   :reading_railroad => {"mortgage_value" => "$100"},
+   :pennsylvania_railroad => {"mortgage_value" => "$200"},
+   :b_and_o_railroad => {"mortgage_value" => "$400"},
+   :shortline_railroad => {"mortgage_value" => "$800"}
+}}}
 end
