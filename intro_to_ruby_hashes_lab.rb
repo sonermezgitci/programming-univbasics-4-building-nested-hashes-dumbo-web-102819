763c2babe4cf47805bd39e4003f016fc0eb28883
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-14 16:48:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,7 @@ def monopoly_with_third_tier
       :one_piece_owned => 25,
       :two_piece_owned => 50,
       :three_piece_owned => 100,
-      :four_piece_owned => 200
-  }
+      :four_piece_owned => 200}
 end
   :names => 4,
 
