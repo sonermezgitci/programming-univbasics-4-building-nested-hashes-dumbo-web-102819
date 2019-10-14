ca6db5770066d97185e1b7abeeffdd8692232b0f
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-14 15:20:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,18 +1,5 @@
 def base_hash
-countries_and_capitals_of_the_world = {
-  :north_america => {
-    "Canada"=>{capital: "Ottawa", capital_climate: "Kőppen Dfb"
-     },
-    "USA" => {capital: "Washington D.C.", capital_climate: "Kőppen Cfa"
-       },
-  },
-  :africa =>{
-    "Ghana"=> {capital: "Accra", capital_climate: "Kőppen Aw"
-  },
-    "Nigeria"=> {capital: "Abuja", capital_climate: "Kőppen Aw"
-      }
-  }
-}
+{}
 	
 end
 
