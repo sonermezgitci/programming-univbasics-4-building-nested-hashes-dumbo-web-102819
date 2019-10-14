c5e5434c492248a957fd8c1862f7206310b4ba18
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-14 17:54:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,5 +50,5 @@ def monopoly_with_fourth_tier
    :pennsylvania_railroad => {"mortgage_value" => "$200"},
    :b_and_o_railroad => {"mortgage_value" => "$400"},
    :shortline_railroa => {"mortgage_value" => "$800"}
-  }}
+  }
 end
